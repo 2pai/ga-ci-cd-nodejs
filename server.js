bb06fd5e9cf6eac43ea1511b918d94b0b62e2b21
2pai <iqbalsyamilayas@gmail.com>
2020-09-05 15:22:37 +0700

    add youtube endpoint
    Signed-off-by: 2pai <iqbalsyamilayas@gmail.com>

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const {greeting} = require('./user')
 
 app.get('/', (req,res) => res.send('This App is running properly!'))
 app.get('/ping', (req,res) => res.send('Pong!'))
+app.get('/youtube', (req,res) => res.send('Hello, Youtube!'))
 app.get('/hello/:name', (req,res) => {
     res.json({message:greeting(req.params.name)})
 })
